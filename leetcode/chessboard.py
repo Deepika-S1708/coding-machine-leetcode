dcc3aca9581e2ca1ef716bf736abab7683347234
Deepika Singari Velu <68435141+Deepika-S1708@users.noreply.github.com>
2024-09-02 22:12:59 +0000

    chessboard code

---
+++ leetcode/chessboard.py
@@ -0,0 +1,25 @@
+class Solution:
+    def chessboard(self, inputNum:int):
+        board=[]
+        for i in range(inputNum):
+            row=[]
+            for j in range(inputNum):
+                if (i+j)%2==0:
+                    row.append("W")
+                else:
+                    row.append("B")
+            board.append(" ".join(row))
+        return board
+
+if __name__=="__main__":
+    s=Solution()
+    inputNum=5
+    res= s.chessboard(inputNum)
+    # print(res)
+    for i in res:
+        print(i)
+
+         
+
+
+
